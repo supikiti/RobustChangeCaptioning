0b1c4be8659daeee0cfeef961577dbb6336781df
supikiti <supikiti22@gmail.com>
2021-11-29 06:50:33 +0000

    add dataset and preprocess code for scene graph generation

---
+++ datasets/hag_dataset_with_scene.py
@@ -0,0 +1,102 @@
+import csv
+import os
+import json
+import numpy as np
+import random
+import time
+import pickle
+from pathlib import Path
+
+import h5py
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data.dataloader import default_collate
+
+
+def open_csv_per_line(file_path, is_header = True):
+    with open(file_path, "r") as f:
+        reader = csv.reader(f)
+        if is_header: 
+            next(reader)
+        yield from reader
+
+
+def load_pickle_data(path):
+    with open(path, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+
+class HAGDataset_with_scene(Dataset):
+    def __init__(self, cfg, split, img_feat_base_path):
+        self.cfg = cfg
+        self.img_feat_base_path = Path(img_feat_base_path)
+
+        print('Speaker Dataset loading vocab json file: ', cfg.data.vocab_json)
+        self.vocab_json = cfg.data.vocab_json
+        self.word_to_idx = json.load(open(self.vocab_json, 'r'))
+        self.idx_to_word = {}
+        for word, idx in self.word_to_idx.items():
+            self.idx_to_word[idx] = word
+
+        self.vocab_size = len(self.idx_to_word)
+        print('vocab size is ', self.vocab_size)
+        
+        self.split = split
+
+        split_pickle_path = "hag_data/{}_data.pickle".format(split)
+        self.split_data = load_pickle_data(split_pickle_path)
+        self.split_data = [tmp for tmp in self.split_data if len(tmp) == 4]
+
+        self.num_samples = len(self.split_data)
+
+        if split == 'train':
+            self.batch_size = cfg.data.train.batch_size
+            self.seq_per_img = cfg.data.train.seq_per_img
+        elif split == 'dev': 
+            self.batch_size = cfg.data.val.batch_size
+            self.seq_per_img = cfg.data.val.seq_per_img
+        elif split == 'test': 
+            self.batch_size = cfg.data.test.batch_size
+            self.seq_per_img = cfg.data.test.seq_per_img
+        else:
+            raise Exception('Unknown data split %s' % split)
+
+        seq_size = len(self.split_data[0][-1])
+        self.max_seq_length = seq_size
+        print('Max sequence length is %d' % self.max_seq_length)
+
+    def __len__(self):
+        return self.num_samples
+
+    def __getitem__(self, i):
+        random.seed()
+
+        idx, img_1_img_path, img_2_img_path, cap_data = self.split_data[i]
+
+        img_1_base_path = Path(img_1_img_path).name
+        img_2_base_path = Path(img_2_img_path).name
+
+        img_1_npy_path = str(self.img_feat_base_path / (img_1_base_path + ".npy"))
+        img_2_npy_path = str(self.img_feat_base_path / (img_2_base_path + ".npy"))
+
+        img_1_feature = torch.from_numpy(np.load(img_1_npy_path))
+        img_2_feature = torch.from_numpy(np.load(img_2_npy_path))
+
+        cap_data = torch.from_numpy(np.array(cap_data, dtype=np.int))
+        mask = (cap_data != 0)
+
+        return img_1_feature, img_2_feature, cap_data, mask, \
+            img_1_img_path, img_2_img_path
+
+    def get_vocab_size(self):
+        return self.vocab_size
+
+    def get_idx_to_word(self):
+        return self.idx_to_word
+
+    def get_word_to_idx(self):
+        return self.word_to_idx
+
+    def get_max_seq_length(self):
+        return self.max_seq_length
---
+++ src-hag/preprocess_captions_with_scene.py
@@ -0,0 +1,186 @@
+import csv
+import pickle
+from pathlib import Path
+import sys
+import os
+sys.path.insert(0, os.path.abspath('.'))
+import json
+import argparse
+import random
+import h5py
+import numpy as np
+
+from janome.tokenizer import Tokenizer
+
+from collections import defaultdict
+from utils.preprocess import tokenize, encode, build_vocab
+from utils.preprocess import tokenize_jp, encode_jp, build_vocab_jp
+
+
+def parser():
+    # Load config
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--img_cap_pair_file_path", type = str, \
+        default = "/mnt/home/taiki-n/riken/data/home-action-genome/true_res/new_true_dataset_with_scene.csv")
+    parser.add_argument('--input_vocab_json', default=None)
+    parser.add_argument('--output_vocab_json', default='hag_data_with_scene/vocab.json', help='output vocab file')
+    parser.add_argument('--output_pickle_dir', default="hag_data_with_scene", help='output h5 file')
+    parser.add_argument('--word_count_threshold', default=1, type=int)
+    parser.add_argument('--allow_unk', default=0, type=int)
+
+    args = parser.parse_args()
+    return args
+
+
+def open_img_cap_pair_csv(path):
+    with open(path, "r") as f:
+        reader = csv.reader(f)
+        yield from reader
+
+
+def split_cap_data(data_list):
+    length = len(data_list)
+    idxs = [int(length * num) for num in [0.85, 0.05, 0.1]]
+
+    return_data = []
+    data_len = 0
+    for i, idx in enumerate(idxs):
+        return_data.append(data_list[data_len : data_len + idx])
+        data_len += idx
+
+    train_list, dev_list, eval_list = return_data
+    return train_list, dev_list, eval_list
+
+
+def get_cap_part(data_list):
+    for data_line in data_list:
+        yield data_line[-1]
+
+
+def get_idx_to_img_pair_data_path(img_cap_pair, img_data_base_path):
+    for idx, data in enumerate(img_cap_pair):
+        data_1_path = Path(img_data_base_path) / (data[0] + ".jpg.npy")
+        data_2_path = Path(img_data_base_path) / (data[1] + ".jpg.npy")
+
+        yield [idx, str(data_1_path), str(data_2_path), data[-1]]
+
+
+def get_cap_part_from_dict(idx_to_img_pair_gen):
+    for *other, cap in idx_to_img_pair_gen:
+        yield [other, data[-1]]
+
+
+def add_idx(dataset_list):
+    for idx, data in enumerate(dataset_list):
+        yield [idx, *data]
+
+
+def get_list_and_cap_data_from_path(dataset_path):
+
+    dataset_pair_list = list(open_img_cap_pair_csv(dataset_path))
+    train_list, dev_list, eval_list = split_cap_data(dataset_pair_list)
+    train_list = list(add_idx(train_list))
+    dev_list = list(add_idx(dev_list))
+    eval_list = list(add_idx(eval_list))
+
+    all_idx_cap = train_list + dev_list + eval_list
+
+    return all_idx_cap, train_list, dev_list, eval_list
+
+
+def tokenize_generator(captions, tokenizer=None):
+    for *others, cap in captions:
+        cap_tokens = tokenize_jp(cap,
+                                add_start_token=True,
+                                add_end_token=True,
+                                punct_to_remove=[",", "，", "、", ".", "。", "．"],
+                                tokenizer=tokenizer)
+        yield [*others, cap_tokens]
+
+
+def get_max_length(cap_tokens):
+    max_length = -1
+    for *othres, cap_token in cap_tokens:
+        if max_length < len(cap_token):
+            max_length = len(cap_token) 
+
+    return max_length
+
+
+def get_encoded_data(cap_tokens, word_to_idx):
+    cap_tokens = sorted(cap_tokens, key=lambda x:int(x[0]))
+    max_length = get_max_length(cap_tokens)
+    
+    for *others, tokens_list in cap_tokens:
+        Li = np.zeros(max_length, dtype=np.int)
+        tokens_encoded = encode_jp(tokens_list,
+                                word_to_idx,
+                                allow_unk=args.allow_unk == 1)
+
+        for k, w in enumerate(tokens_encoded):
+            Li[k] = w
+
+        yield [*others, list(Li)]
+
+
+def save_data_as_pickle(saved_data, path):
+    with open(path, "wb") as f:
+        pickle.dump(saved_data, f)
+
+
+def load_pickle_data(path):
+    with open(path, "rb") as f:
+        saved_data = pickle.load(f)
+    return saved_data
+
+
+def main(args):
+    all_data, train_data, dev_data, eval_data = \
+        get_list_and_cap_data_from_path(args.img_cap_pair_file_path)
+
+    ## Either create the vocab or load it from disk
+    if args.input_vocab_json is None:
+        print('Building vocab')
+        word_to_idx = build_vocab_jp(
+            all_data,
+            min_token_count=args.word_count_threshold,
+            punct_to_remove=[",", "，", "、", ".", "。", "．"]
+        )
+    else:
+        print('Loading vocab')
+        with open(args.input_vocab_json, 'r') as f:
+            word_to_idx = json.load(f)
+
+    if args.output_vocab_json is not None:
+        with open(args.output_vocab_json, 'w') as f:
+            json.dump(word_to_idx, f)
+
+    # Encode all captions
+    # First, figure out max length of captions
+    t = Tokenizer()
+
+    all_cap_tokens = list(tokenize_generator(all_data, t))
+    train_cap_tokens = list(tokenize_generator(train_data, t))
+    dev_cap_tokens = list(tokenize_generator(dev_data, t))
+    eval_cap_tokens = list(tokenize_generator(eval_data, t))
+
+    #all_cap_encoded = get_encoded_data(all_cap_tokens, word_to_idx)
+    train_encoded = list(get_encoded_data(train_cap_tokens, word_to_idx))
+    dev_encoded = list(get_encoded_data(dev_cap_tokens, word_to_idx))
+    eval_encoded = list(get_encoded_data(eval_cap_tokens, word_to_idx))
+
+    output_train_pickle_path = str(Path(args.output_pickle_dir) / "train_data.pickle")
+    output_dev_pickle_path = str(Path(args.output_pickle_dir) / "dev_data.pickle")
+    output_eval_pickle_path = str(Path(args.output_pickle_dir) / "eval_data.pickle")
+
+    save_data_as_pickle(train_encoded, output_train_pickle_path)
+    save_data_as_pickle(dev_encoded, output_dev_pickle_path)
+    save_data_as_pickle(eval_encoded, output_eval_pickle_path)
+
+    print("FINISH ENCODING !!!")
+        
+
+if __name__ == '__main__':
+    args = parser()
+    main(args)
